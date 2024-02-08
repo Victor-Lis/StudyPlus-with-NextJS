b7680adbaf92a20398c7c6616238f9a68a2a3811
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-07 21:10:49 -0300

    v1.16
    - Recebendo tarefas na tela.

--- src/app/tarefas/page.tsx
+++ src/app/tarefas/page.tsx
@@ -1,7 +1,29 @@
+"use client"
+
+import prisma from '@/lib/prisma'
+import { Tarefa } from '@/@types/tarefa';
+import { getTasks } from './utils/get';
+import { useEffect, useState } from 'react';
+
 export default function Tarefas() {
+
+  const [tarefas, setTarefas] = useState<Tarefa[]>([]);
+
+  async function handleGetTasks(){
+    let data = await getTasks()
+    setTarefas(data)
+  }
+
+  useEffect(() => {
+    handleGetTasks()
+  }, [])
+
  return (
-   <div>
+   <div className="flex items-center flex-col justify-center min-h-[calc(100vh-80px)] bg-zinc-900">
     <h2>Tarefas</h2>
+    {tarefas.map((tarefa) => {
+      return <h2>{tarefa.title}</h2>
+    })}
    </div>
  );
 }
