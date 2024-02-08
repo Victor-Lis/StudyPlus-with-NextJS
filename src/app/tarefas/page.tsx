c1581c76b493d482bd2ebb456212cf945083c29b
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-07 22:16:22 -0300

    v1.24
    - Criando context para manipular tarefas

--- src/app/tarefas/page.tsx
+++ src/app/tarefas/page.tsx
@@ -5,32 +5,35 @@ import { getTasks } from "./utils/get";
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import GridTarefas from "./components/GridTarefas";
-import Tarefa from './components/Tarefa'
+import Tarefa from "./components/Tarefa";
 import { verifyAuth } from "../utils/verifyAuth";
+import { TarefaProvider } from "./providers/tarefaContext";
 
 export default function Tarefas() {
   const [tarefas, setTarefas] = useState<TarefaType[]>([]);
 
   async function handleGetTasks() {
-      let data = await getTasks();
-      if(data){
-        setTarefas(data);
-      }
+    let data = await getTasks();
+    if (data) {
+      setTarefas(data);
+    }
   }
 
   useEffect(() => {
-    verifyAuth()
+    verifyAuth();
     handleGetTasks();
   }, []);
 
   return (
-    <div className="min-h-[calc(100vh-80px)] bg-zinc-900">
-      <Header />
-      <GridTarefas>
-        {tarefas.map((tarefa) => {
-          return <Tarefa key={tarefa.id} tarefa={tarefa}/>
-        })}
-      </GridTarefas>
-    </div>
+    <TarefaProvider>
+      <div className="min-h-[calc(100vh-80px)] bg-zinc-900">
+        <Header />
+        <GridTarefas>
+          {tarefas.map((tarefa) => {
+            return <Tarefa key={tarefa.id} tarefa={tarefa} />;
+          })}
+        </GridTarefas>
+      </div>
+    </TarefaProvider>
   );
 }
