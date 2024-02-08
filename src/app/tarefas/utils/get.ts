4ea034bf73bec006678adf9d49372cbd0423d621
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-07 21:29:13 -0300

    v1.18
    - Ajustando função getTasks

--- src/app/tarefas/utils/get.ts
+++ src/app/tarefas/utils/get.ts
@@ -1,10 +1,14 @@
 "use server"
 
 import prisma from '@/lib/prisma'
-import { Tarefa } from '@/@types/tarefa';
+import { TarefaType } from '@/@types/tarefa';
 
 export async function getTasks(){
-    let tarefas: Tarefa[] = await prisma.task.findMany()
+    let tarefas: TarefaType[] = await prisma.task.findMany({
+        include: {
+            Categorie: true
+        }
+    })
     tarefas.sort((a,b) => a.date.getTime()-b.date.getTime())
     return tarefas
 }
