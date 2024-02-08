32245737717ea370a4b96252d78b74fd3a2452ed
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-07 21:52:19 -0300

    v1.21
    - Criando where para verificar tasks.

--- src/app/tarefas/utils/get.ts
+++ src/app/tarefas/utils/get.ts
@@ -2,13 +2,24 @@
 
 import prisma from '@/lib/prisma'
 import { TarefaType } from '@/@types/tarefa';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
 
 export async function getTasks(){
+
+    const session = await getServerSession(authOptions);
+
     let tarefas: TarefaType[] = await prisma.task.findMany({
+        where: {
+            User: {
+                email: session?.user?.email
+            }
+        },
         include: {
-            Categorie: true
+            Categorie: true,
         }
     })
     tarefas.sort((a,b) => a.date.getTime()-b.date.getTime())
+
     return tarefas
 }
