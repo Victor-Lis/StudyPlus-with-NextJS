98cfc83be04eb34f19d3728162a63ac574b5da4d
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-10 18:56:45 -0300

    v1.61
    - Criando Create e Edit forms e suas funções respectivas

---
+++ src/app/semana/dia/[dia]/components/Tarefas/modalTarefas/components/createForm/index.tsx
@@ -0,0 +1,136 @@
+"use client";
+import {
+  useEffect,
+  useState,
+  FormEvent,
+  useContext,
+  Dispatch,
+  SetStateAction,
+} from "react";
+import { createTask, getCategories } from "../../utils/functions";
+import { useRouter } from "next/navigation";
+import { FaSpinner } from "react-icons/fa";
+import { ModalTaskContext } from "../../../providers/modalTaskProvider";
+import { CategoriaType } from "@/@types/categoria";
+
+export default function CreateForm({
+  toggleModal,
+}: {
+  toggleModal: () => void;
+}) {
+  const { dayId } = useContext(ModalTaskContext);
+  const [title, setTitle] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
+  const [primeiraHora, setPrimeiraHora] = useState<string>("");
+  const [ultimaHora, setUltimaHora] = useState<string>("");
+  const [categories, setCategories] = useState<CategoriaType[]>([])
+  const [categorie, setCategorie] = useState<number | undefined>()
+
+  const route = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  async function handleCreateTask(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    if (!!title && !!desc && primeiraHora && !!ultimaHora && !!categorie) {
+      setLoading(true);
+      let novaTarefa = await createTask({
+        title,
+        desc,
+        primeira_hora: primeiraHora,
+        ultima_hora: ultimaHora,
+        categorieId: categorie as number,
+        dayId,
+      });
+
+      if (novaTarefa) {
+        route.refresh();
+        toggleModal();
+      }
+    }
+    setLoading(false);
+  }
+
+  async function handleGetCategories(){
+    let data = await getCategories()
+    if(data){
+      setCategorie(data[0].id)
+      setCategories(data)
+    }
+  }   
+
+  useEffect(() => {
+    handleGetCategories()
+  }, [])
+
+  return (
+    <>
+      <h2 className="text-green-500 text-2xl w-full">Cadastrar Tarefa</h2>
+      <form
+        className="flex flex-col justify-center items-center text-white"
+        onSubmit={(e) => handleCreateTask(e)}
+      >
+        <div className="mt-10 w-10/12 mx-auto flex flex-col items-center justify-between">
+          <div className="flex items-center justify-between w-full">
+            <h2 className="mr-2 flex-1">Título</h2>
+            <input
+              type="text"
+              className="border-b-2 border-b-gray-300 px-2 ml-5 w-[48.5%] rounded text-black"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between w-full mt-10">
+            <h2 className="mr-2 flex-1">Descrição</h2>
+            <textarea
+              className="border-b-2 border-b-gray-300 ml-5 flex-1 min-h-18 px-2 py-1 rounded text-black resize-none"
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between w-full mt-10">
+            <h2 className="mr-2 flex-1">Começo</h2>
+            <input
+              type="time"
+              className="border-b-2 border-b-gray-300 px-2 ml-5 flex-1 rounded text-black"
+              value={primeiraHora}
+              onChange={(e) => setPrimeiraHora(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between w-full mt-10">
+            <h2 className="mr-2 flex-1">Fim</h2>
+            <input
+              type="time"
+              className="border-b-2 border-b-gray-300 px-2 ml-5 flex-1 rounded text-black"
+              value={ultimaHora}
+              onChange={(e) => setUltimaHora(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center justify-between w-full mt-10">
+            <h2 className="mr-2 flex-1">Categoria</h2>
+            <select className="border-b-2 border-b-gray-300 w-[48%] rounded text-black" onChange={(e) => setCategorie(parseInt(e.target.value))}>
+              {categories.map((categoria) => {
+                return <option value={categoria.id} className={`bg-[${categoria.color}]`}>{categoria.title}</option>
+              })}
+            </select>
+          </div>
+        </div>
+        <button
+          disabled={loading}
+          className={
+            !loading
+              ? "bg-green-500 mt-10 mx-auto py-1 px-5 hover:text-white hover:scale-105 duration-300 rounded"
+              : "bg-gray-500 opacity-75 scale-75 mt-10 mx-auto py-1 px-10 text-white rounded"
+          }
+        >
+          {loading ? (
+            <FaSpinner className="animate-spin" color="#fff" size={25} />
+          ) : (
+            "Cadastrar!"
+          )}
+        </button>
+      </form>
+    </>
+  );
+}
---
+++ src/app/semana/dia/[dia]/components/Tarefas/modalTarefas/utils/functions.ts
@@ -0,0 +1,210 @@
+"use server";
+
+import { CategoriaType } from "@/@types/categoria";
+import { DayType } from "@/@types/dia";
+import { TarefaType } from "@/@types/tarefa";
+import { WeekType } from "@/@types/week";
+import { authOptions } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+
+export async function sumHours(task: TarefaType){
+    let day = await prisma.day.findFirst({
+        where: {
+            id: task.day
+        }
+    }) as DayType
+
+    await prisma.day.update({
+        where: {
+            id: day.id
+        },
+        data: {
+            hours: day.hours + task.hours
+        }
+    })
+
+    let week = await prisma.week.findFirst({
+        where: {
+            id: day.week
+        }
+    }) as WeekType
+
+    await prisma.week.update({
+        where: {
+            id: week.id
+        },
+        data: {
+            hours: week.hours + task.hours
+        }
+    })
+}
+
+export async function subHours(task: TarefaType){
+
+    let day = await prisma.day.findFirst({
+        where: {
+            id: task.day
+        }
+    }) as DayType
+
+    await prisma.day.update({
+        where: {
+            id: day.id
+        },
+        data: {
+            hours: day.hours - task.hours
+        }
+    })
+
+    let week = await prisma.week.findFirst({
+        where: {
+            id: day.week
+        }
+    }) as WeekType
+
+    await prisma.week.update({
+        where: {
+            id: week.id
+        },
+        data: {
+            hours: week.hours - task.hours
+        }
+    })
+    
+}
+
+async function createTask({
+  title,
+  desc,
+  primeira_hora,
+  ultima_hora,
+  categorieId,
+  dayId,
+}: {
+  title: string;
+  desc: string;
+  primeira_hora: string;
+  ultima_hora: string;
+  categorieId: number;
+  dayId: number;
+}) {
+  const session = await getServerSession(authOptions);
+
+  if (ultima_hora == "00:00") {
+    ultima_hora = "24:00";
+  }
+
+  let firstHour = parseInt(primeira_hora.slice(0, 2)) * 60 + parseInt(primeira_hora.slice(3, 5));
+  let lastHour = parseInt(ultima_hora.slice(0, 2)) * 60 + parseInt(ultima_hora.slice(3, 5));
+
+  const task = await prisma.task.create({
+    data: {
+      title,
+      desc,
+      primeira_hora,
+      ultima_hora,
+      hours: parseFloat((lastHour - firstHour).toFixed(1)),
+      Day: {
+        connect: {
+          id: dayId,
+        }
+      },
+      Categorie: {
+        connect: {
+          id: categorieId,
+        }
+      },
+      User: {
+        connect: {
+          email: session?.user?.email as string,
+        },
+      },
+    },
+  });
+
+  sumHours(task)
+
+  return task;
+}
+
+async function updateTask({
+  id,
+  title,
+  desc,
+  primeira_hora,
+  ultima_hora,
+  categorieId,
+  dayId,
+}: {
+  id: number,
+  title: string;
+  desc: string;
+  primeira_hora: string;
+  ultima_hora: string;
+  categorieId: number;
+  dayId: number;
+}) {
+  const session = await getServerSession(authOptions);
+
+  const initTask = await prisma.task.findFirst({
+    where: {
+      id,
+    }
+  }) as TarefaType
+
+  subHours(initTask)
+
+  if (ultima_hora == "00:00") {
+    ultima_hora = "24:00";
+  }
+
+  let firstHour = parseInt(primeira_hora.slice(0, 2)) * 60 + parseInt(primeira_hora.slice(3, 5));
+  let lastHour = parseInt(ultima_hora.slice(0, 2)) * 60 + parseInt(ultima_hora.slice(3, 5));
+
+  const task = await prisma.task.update({
+    where: {
+      id,
+    },
+    data: {
+      title,
+      desc,
+      primeira_hora,
+      ultima_hora,
+      hours: parseFloat((lastHour - firstHour).toFixed(1)),
+      Day: {
+        connect: {
+          id: dayId,
+        }
+      },
+      Categorie: {
+        connect: {
+          id: categorieId,
+        }
+      },
+      User: {
+        connect: {
+          email: session?.user?.email as string,
+        },
+      },
+    },
+  });
+
+  sumHours(task)
+
+  return task;
+}
+
+async function getCategories() {
+  const session = await getServerSession(authOptions);
+
+  return (await prisma.categorie.findMany({
+    where: {
+      User: {
+        email: session?.user?.email,
+      },
+    },
+  })) as CategoriaType[];
+}
+
+export { createTask, updateTask, getCategories };
