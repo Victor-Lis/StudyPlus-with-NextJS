b0a8b697061eee24ef49b365af83b474d04243bf
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-09 23:07:16 -0300

    v1.50
    - Chamando handleModalVisible, para abrir o modal

--- src/app/semana/dia/[dia]/components/Categorias/components/Button/index.tsx
+++ src/app/semana/dia/[dia]/components/Categorias/components/Button/index.tsx
@@ -1,8 +1,15 @@
 "use client"
+import { useContext } from "react";
 import { FiPlusCircle } from "react-icons/fi";
+import { ModalCategorieContext } from "../../../../providers/modalCategorieProvider";
 
 export default function Button() {
+
+   const { handleModalVisible } = useContext(ModalCategorieContext)
+
  return (
-    <FiPlusCircle color="rgb(37 235 99)" size={35} className="hover:scale-105 duration-300 cursor-pointer" onClick={() => console.log("Criando categoria")}/>
+    <FiPlusCircle color="rgb(37 235 99)" size={35} className="hover:scale-105 duration-300 cursor-pointer" onClick={() => {
+      handleModalVisible()
+    }}/>
  );
 }
--- src/app/semana/dia/[dia]/components/Tarefas/components/Button/index.tsx
+++ src/app/semana/dia/[dia]/components/Tarefas/components/Button/index.tsx
@@ -1,8 +1,11 @@
 "use client"
+import { useContext } from "react";
 import { FiPlusCircle } from "react-icons/fi";
 
 export default function Button() {
+
+
  return (
-    <FiPlusCircle color="rgb(37 235 99)" size={35} className="hover:scale-105 duration-300 cursor-pointer" onClick={() => console.log("Criando tarefa")}/>
+    <FiPlusCircle color="rgb(37 235 99)" size={35} className="hover:scale-105 duration-300 cursor-pointer"/>
  );
 }
