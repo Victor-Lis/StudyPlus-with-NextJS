759dee8e4e4390dcc9dcf34f440cf6451e4dc93c
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-09 23:39:53 -0300

    v1.53
    - Adicionando features de alterar / apagar categorias.

---
+++ src/app/semana/dia/[dia]/components/Categorias/components/Categorie/index.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { CategoriaType } from "@/@types/categoria";
+import { useContext, useState } from "react";
+import { FiEdit, FiTrash } from "react-icons/fi";
+import { ModalCategorieContext } from "../../../../providers/modalCategorieProvider";
+import { deleteCategorie } from "../../utils/functions";
+import { useRouter } from "next/navigation";
+
+export default function Categorie({ categorie }: { categorie: CategoriaType }) {
+  const { setEditCategoria } = useContext(ModalCategorieContext)
+  const [loading, setLoading] = useState<boolean>(false)
+  const route = useRouter()
+
+  async function handleDeleteCategorie(){
+    setLoading(true)
+    await deleteCategorie({id: categorie.id})
+    route.refresh()
+    setLoading(false)
+  }
+
+  return (
+    <div
+      className={
+        !loading ? 
+         `w-full bg-[${categorie.color}] flex flex-col justify-center items-center rounded p-2`
+        : 
+         `w-full bg-gray-500 flex flex-col justify-center items-center rounded p-2`
+      }
+    >
+      <h2 className="text-2xl my-5">{categorie.title}</h2>
+      <div className="w-full flex items-center justify-around mb-5">
+        <FiEdit className="hover:scale-110 duration-300 cursor-pointer" color="#00ff00" size={27} onClick={() => {setEditCategoria(categorie)}}/>
+        <FiTrash className="hover:scale-110 duration-300 cursor-pointer" color="#ff0000" size={30} onClick={() => handleDeleteCategorie()}/>
+      </div>
+    </div>
+  );
+}
