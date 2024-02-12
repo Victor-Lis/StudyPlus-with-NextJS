193b3903d04bb5965d05c0269bd2924888ad9d2c
Victor-Lis <victorlisbronzo1@gmail.com>
2024-02-12 19:27:33 -0300

    v1.84
    - Anteriormente caso a categoria tivesse tarefas não era possível deleta-las, agora é possível

--- src/@types/categoria.ts
+++ src/@types/categoria.ts
@@ -1,7 +1,10 @@
+import { TarefaType } from "./tarefa";
+
 export interface CategoriaType{
     id: number;
     title: string;
     text_color: string;
     bg_color: string;
     user_id: string;
+    task: TarefaType[]
 }
--- src/app/semana/dia/[dia]/components/Categorias/utils/functions.ts
+++ src/app/semana/dia/[dia]/components/Categorias/utils/functions.ts
@@ -1,18 +1,47 @@
 "use server";
 
+import { CategoriaType } from "@/@types/categoria";
 import { authOptions } from "@/lib/auth";
 import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
+
+async function deleteTask({categorie}:{categorie: CategoriaType}){
+  categorie?.task.map(async (task, index) => {
+    await prisma.task.delete({
+      where: {
+        id: task.id,
+      },
+    });
+
+    categorie.task.filter((t) => task.id !== t.id)
+  })
+
+  if(categorie.task.length === 0){
+    return true
+  }
+}
 
 async function deleteCategorie({
     id,
   }: {
     id: number;
   }) {
+
+    const initCategorie = await prisma.categorie.findFirst({
+      where: {
+        id,
+      },
+      include: {
+        task: true,
+      }
+    }); 
+
+    await deleteTask({categorie: initCategorie as CategoriaType})
+
     const categorie = await prisma.categorie.delete({
       where: {
         id,
-      }
+      },
     });
   
     return categorie;
